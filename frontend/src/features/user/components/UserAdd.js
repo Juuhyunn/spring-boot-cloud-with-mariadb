e48311d83a5b5f58b4a584c97fd5b5222143bd3b
Juuhyunn <ahnju96@gmail.com>
2021-10-30 15:49:16 +0900

    30, October

--- frontend/src/features/user/components/UserAdd.js
+++ frontend/src/features/user/components/UserAdd.js
@@ -1,83 +1,112 @@
-
 import React, { useState, useCallback } from 'react';
 import { useHistory  } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { joinPage } from 'features/user/reducer/userSlice'
 import { useForm } from "react-hook-form";
-
-
+import styled from 'styled-components'
 
 export default function UserAdd() {
     const history = useHistory()
     const dispatch = useDispatch()
-    const { register } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    // https://react-hook-form.com/kr/advanced-usage/
     const [join, setJoin] = useState({
         username:'', password:'', email:'', name:'', regDate: new Date().toLocaleDateString()
     })
+
     const {username, password, email, name} = join
-    const handleChange = useCallback(
-        e => {
-            const { value, name } = e.target
-            setJoin({
-                ...join,
-                [name] : value
-            })
-        }, [join]
-    ) 
+    const handleChange = useCallback(()=> {
+        const { value, name } = e.target
+        setJoin({
+            ...join,
+            [name] : value
+        })
+    }, [join]) 
     
-    const handleSubmit = async(e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        //캡처링(부모로 데이터 전달) 과 버블링(부모로부터 데이터 전달) 을 막기위함
-        alert(`회원가입 ID 1: ${join.username}`)
-        const json = {
+    const onSubmit = async () => {
+  
+        await dispatch(joinPage( {
             'username': join.username,
             'password': join.password,
             'email': join.email,
             'name': join.name,
             'regDate': join.regDate
-        }
-        alert(`회원가입 정보: ${JSON.stringify(json)}`)
-        await dispatch(joinPage(json))
+        }))
         alert(`${join.username} 회원가입 환영`)
         history.push('/users/login')
-
   }
 
   return (
     <div>
-         <h1>회원 가입을 환영합니다.</h1>
-    <form onSubmit={handleSubmit} method='POST'>
+         <h1>회원 가입</h1>
+    <form method='POST' onSubmit={handleSubmit(onSubmit)}>
         <ul>
             <li>
-                <label>
-                    아이디 : <input type="text" id="username" name="username" value={username} onChange={handleChange}
-                    size="10" minlength="4" maxlength="15" {...register('username', { required: true, maxLength: 30 })}/>
-                </label>
+                <label>아이디 : </label>
+                    {/* use aria-invalid to indicate field contain error */}
+                <input type="text" id="username" 
+                    {...register('username', { required: true, maxLength: 30 })}
+                    name="username" value={username} onChange={handleChange}
+                    size="10" minlength="4" maxlength="15"/>
+                    {/* use role="alert" to announce the error message */}
+                    {errors.username && errors.username.type === "required" && (
+                        <Span role="alert">아이디는 필수값입니다</Span>
+                    )}
+                    {errors.username && errors.username.type === "maxLength" && (
+                        <Span role="alert">아이디는 4자에서 15 글자이어야 합니다.</Span>
+                    )}
+                <br/>
                 <small>4~15자리 이내의 영문과 숫자</small>
             </li>
             <li>
-                <label>
-                    이메일 : <input type="email" id="email" name="email" value={email} onChange={handleChange}/>
-                </label>
+                <label> 이메일 : </label>
+                <input type="email" id="email" 
+                    aria-invalid={errors.name ? "true" : "false"}
+                    {...register('email', { required: true, maxLength: 30 })}
+                    name="email" value={email} onChange={handleChange}
+                    size="10" minlength="4" maxlength="15"/>
+                {/* use role="alert" to announce the error message */}
+                {errors.email && errors.email.type === "required" && (
+                    <Span role="alert">아이디는 필수값입니다</Span>
+                )}
+                {errors.email && errors.email.type === "maxLength" && (
+                    <Span role="alert">아이디는 4자에서 15 글자이어야 합니다.</Span>
+                )}
             </li>
             <li>
-                <label>
-                    비밀 번호 : <input type="password" id="password" name="password" value={password} onChange={handleChange}/>
-                </label>
+                <label>비밀 번호 : </label>
+                <input type="password" id="password" 
+                    aria-invalid={errors.password ? "true" : "false"}
+                    {...register('password', { required: true, maxLength: 30 })}
+                    name="password" value={password} onChange={handleChange}
+                    size="10" minlength="1" maxlength="15"/>
+                {errors.password && errors.password.type === "required" && (
+                    <Span role="alert">비밀 번호는 필수값입니다</Span>
+                )}
+                {errors.password && errors.password.type === "maxLength" && (
+                    <Span role="alert">비밀 번호는 1자에서 15 글자이어야 합니다.</Span>
+                )}
             </li>
             <li>
-                <label>
-                    이름 : <input type="text" id="name" name="name" value={name} onChange={handleChange}/>
-                </label>
+                <label> 이름 : </label>
+                <input type="text" id="name" 
+                    aria-invalid={errors.name ? "true" : "false"}
+                    {...register('name', { required: true, maxLength: 30 })}
+                    name="name" value={name} onChange={handleChange}
+                    size="10" minlength="4" maxlength="15"/>
+                {errors.name && errors.name.type === "required" && (
+                    <Span role="alert">이름은 필수값입니다</Span>
+                )}
+                {errors.name && errors.name.type === "maxLength" && (
+                    <Span role="alert">이름은 4자에서 15 글자이어야 합니다.</Span>
+                )}
             </li>
-           
-            <li>
-                <input type="submit" onClick={ e => handleSubmit(e)} value="회원가입"/>
-            </li>
-
         </ul>
+        <input type="submit" value="회원가입"/> 
     </form>
     </div>
   );
 }
+const Span = styled.span`
+    color: red
+`
